9e73b0e2b5a5f6eb1a2d2c9b78c66dfc31164a1a
Pulpwood <mateusz.przybyla12@gmail.com>
2021-10-27 09:34:39 +0200

    With difficulty levels-corrected

--- settings.py
+++ settings.py
@@ -26,11 +26,11 @@ class Settings:
 
 		self.initialize_dynamic_settings()
 
-	# def initialize_dynamic_settings(self):
-	# 	self.ship_speed = 1.5
-	# 	self.bullet_speed = 3.0
-	# 	self.alien_speed = 1.0
-	# 	self.fleet_direction = 1
+	def initialize_dynamic_settings(self):
+		self.ship_speed = 1.5
+		self.bullet_speed = 3.0
+		self.alien_speed = 1.0
+		self.fleet_direction = 1
 
 	def initialize_dynamic_settings_easy(self):
 		self.ship_speed = 1.5
